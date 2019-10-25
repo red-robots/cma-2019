c5c74f4e508cb102a29a4c7fb109d1d4935b1b5f
hermie <hermiebarit@gmail.com>
2019-10-25 20:41:43 +0800

    Modifying top menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,13 +9,18 @@ jQuery(document).ready(function ($) {
 
 	var slider_height = $('.n2-section-smartslider').height();
 	console.log('Slider Height: ' + slider_height);
-	$('.mobilemenu').height(slider_height + 'px');
+	if( slider_height > 0 ){
+		$('.mobilemenu').height(slider_height + 'px');
+	} else {
+		$('.mobilemenu').fadeToggle(500).toggleClass('hide');
+	}
+	
 	
 	$('.gfield_html_formatted').css('padding-top', '20px');
 	$('.ginput_container_email').css('margin-top', '-22px');
-	
+
 	if (window.matchMedia('(max-width: 768px)').matches) {
-        $('.gform_button').css('width', '39%').css('margin', '0');
+        $('.gform_button').css('width', '26%').css('margin', '0');
     }
 	//
 
